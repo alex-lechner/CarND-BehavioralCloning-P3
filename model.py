b04fa77324cb6e90e24283a6789990d09b0ec18e
ALEX-PC\Alexa <office@alexanderlechner.com>
2017-07-23 22:34:44 +0200

    optimized model architecture and added data visualization picture

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ matplotlib.use('agg')
 
 import matplotlib.pyplot as plt
 import numpy as np
-from keras.layers import Flatten, Dense, Lambda, Cropping2D, Conv2D, Dropout
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Conv2D, Dropout, MaxPooling2D
 from keras.models import Sequential
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
@@ -42,10 +42,10 @@ def generator(samples, batch_size=32):
                     measurement = float(batch_sample[3])
                     if i == 1:
                         # left camera with correction
-                        measurement = measurement + 0.2
+                        measurement = measurement + 0.3
                     if i == 2:
                         # right camera with correction
-                        measurement = measurement - 0.2
+                        measurement = measurement - 0.3
                     measurements.append(measurement)
 
             augmented_images, augmented_measurements = [], []
@@ -60,11 +60,12 @@ def generator(samples, batch_size=32):
             yield shuffle(X_train, y_train)
 
 
-train_generator = generator(train_samples, batch_size=32)
-validation_generator = generator(validation_samples, batch_size=32)
-
 input_shape = (160, 320, 3)
 BATCH_SIZE = 32
+EPOCHS = 3
+
+train_generator = generator(train_samples, batch_size=BATCH_SIZE)
+validation_generator = generator(validation_samples, batch_size=BATCH_SIZE)
 
 # Model architecture
 model = Sequential()
@@ -77,15 +78,16 @@ model.add(Cropping2D(cropping=((50, 20), (0, 0))))
 model.add(Conv2D(24, (5, 5), activation="relu", strides=(2, 2)))
 model.add(Conv2D(36, (5, 5), activation="relu", strides=(2, 2)))
 model.add(Conv2D(48, (5, 5), activation="relu", strides=(2, 2)))
-model.add(Conv2D(64, (3, 3), activation="relu", strides=(2, 2)))
-model.add(Conv2D(64, (3, 3), activation="relu", strides=(2, 2)))
-model.add(Dropout(0.5))
+model.add(Conv2D(64, (3, 3), activation="relu"))
+model.add(Conv2D(64, (3, 3), activation="relu"))
+model.add(MaxPooling2D())
+model.add(Dropout(0.4))
 
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(10))
-model.add(Dropout(0.3))
+model.add(Dropout(0.5))
 
 model.add(Dense(1))
 
@@ -93,7 +95,7 @@ model.compile(loss='mse', optimizer='adam')
 history_obj = model.fit_generator(train_generator, steps_per_epoch=int(len(train_samples) / BATCH_SIZE),
                                   validation_data=validation_generator,
                                   validation_steps=int(len(validation_samples) / BATCH_SIZE),
-                                  nb_epoch=5, verbose=1)
+                                  epochs=EPOCHS, verbose=1)
 model.save('model.h5')
 print('Saved model!')
 

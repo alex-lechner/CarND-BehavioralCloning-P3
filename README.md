10f5de4ef99b436b39c3668210ecaad463089d68
ALEX-PC\Alexa <office@alexanderlechner.com>
2017-07-23 21:30:02 +0200

    optimized model

--- README.md
+++ README.md
@@ -26,9 +26,6 @@ The goals / steps of this project are the following:
 [image6]: ./examples/placeholder_small.png "Normal Image"
 [image7]: ./examples/placeholder_small.png "Flipped Image"
 
-## Rubric Points
-### Here I will consider the [rubric points](https://review.udacity.com/#!/rubrics/432/view) individually and describe how I addressed each point in my implementation.  
-
 ---
 ### Files Submitted & Code Quality
 
@@ -38,7 +35,7 @@ My project includes the following files:
 * model.py containing the script to create and train the model
 * drive.py for driving the car in autonomous mode
 * model.h5 containing a trained convolution neural network 
-* writeup_report.md or writeup_report.pdf summarizing the results
+* README.md summarizing the results
 
 #### 2. Submission includes functional code
 Using the Udacity provided simulator and my drive.py file, the car can be driven autonomously around the track by executing 
@@ -53,11 +50,11 @@ The model.py file contains the code for training and saving the convolution neur
 ### Model Architecture and Training Strategy
 
 #### 1. An appropriate model architecture has been employed
-My mode architecture is based on [Nvidia's End-to-End Deep Learning Model for Self-Driving Cars](https://devblogs.nvidia.com/parallelforall/deep-learning-self-driving-cars/).
+My model architecture is based on [Nvidia's End-to-End Deep Learning Model for Self-Driving Cars](https://devblogs.nvidia.com/parallelforall/deep-learning-self-driving-cars/).
 
-My model consists of a convolution neural network with 3x3 filter sizes and depths between 32 and 128 (model.py lines 18-24) 
+The model consists of a convolutional neural network with 3x3 filter sizes and depths between 32 and 128 (model.py lines 18-24) 
 
-The model includes RELU layers to introduce nonlinearity (code line 20), and the data is normalized in the model using a Keras lambda layer (code line 18). 
+The model , and the data is normalized in the model using a Keras lambda layer (code line 18). 
 
 #### 2. Attempts to reduce overfitting in the model
 
--- model.py
+++ model.py
@@ -2,14 +2,15 @@ import csv
 
 import cv2
 import matplotlib
+
+matplotlib.use('agg')
+
 import matplotlib.pyplot as plt
 import numpy as np
-from keras.layers import Flatten, Dense, Lambda, MaxPooling2D, Cropping2D, Conv2D, Dropout
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Conv2D, Dropout
 from keras.models import Sequential
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
-
-matplotlib.use('agg')
 
 lines = []
 with open('./data/driving_log.csv') as file:
@@ -39,6 +40,12 @@ def generator(samples, batch_size=32):
                     image = cv2.imread(current_path)
                     images.append(image)
                     measurement = float(batch_sample[3])
+                    if i == 1:
+                        # left camera with correction
+                        measurement = measurement + 0.2
+                    if i == 2:
+                        # right camera with correction
+                        measurement = measurement - 0.2
                     measurements.append(measurement)
 
             augmented_images, augmented_measurements = [], []
@@ -57,40 +64,38 @@ train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
 input_shape = (160, 320, 3)
+BATCH_SIZE = 32
 
 # Model architecture
 model = Sequential()
 # Normalizing data
-model.add(Lambda(lambda x: x / 255. - 0.5, input_shape=input_shape, output_shape=input_shape))
+model.add(Lambda(lambda x: (x / 127.5) - 1., input_shape=input_shape, output_shape=input_shape))
 # crop images so we only have the important parts (the street)
 model.add(Cropping2D(cropping=((50, 20), (0, 0))))
+
 # NVIDIA CNN
 model.add(Conv2D(24, (5, 5), activation="relu", strides=(2, 2)))
-# model.add(Dropout(0.2)) # -> old model structure loss:0.0083 val_loss:0.0094
 model.add(Conv2D(36, (5, 5), activation="relu", strides=(2, 2)))
-# model.add(Dropout(0.2)) # -> old model structure loss:0.0083 val_loss:0.0094
 model.add(Conv2D(48, (5, 5), activation="relu", strides=(2, 2)))
-# model.add(Dropout(0.2)) # -> old model structure loss:0.0083 val_loss:0.0094
 model.add(Conv2D(64, (3, 3), activation="relu", strides=(2, 2)))
-# model.add(Dropout(0.2)) # -> old model structure loss:0.0083 val_loss:0.0094
-model.add(Conv2D(64, (3, 3), activation="relu", strides=(2, 2), dim_ordering='th'))
-model.add(MaxPooling2D())
-# model.add(Dropout(0.2)) # -> old model structure loss:0.0083 val_loss:0.0094
-model.add(Flatten())
+model.add(Conv2D(64, (3, 3), activation="relu", strides=(2, 2)))
 model.add(Dropout(0.5))
+
+model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
-model.add(Dropout(0.2))
 model.add(Dense(10))
+model.add(Dropout(0.3))
+
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-history_obj = model.fit_generator(train_generator, steps_per_epoch=len(train_samples),
-                                  validation_data=validation_generator, validation_steps=len(validation_samples),
-                                  nb_epoch=3, verbose=1)
+history_obj = model.fit_generator(train_generator, steps_per_epoch=int(len(train_samples) / BATCH_SIZE),
+                                  validation_data=validation_generator,
+                                  validation_steps=int(len(validation_samples) / BATCH_SIZE),
+                                  nb_epoch=5, verbose=1)
 model.save('model.h5')
-
-print(history_obj.history.keys())
+print('Saved model!')
 
 plt.plot(history_obj.history['loss'])
 plt.plot(history_obj.history['val_loss'])
@@ -99,3 +104,5 @@ plt.ylabel('mean squared error')
 plt.xlabel('epoch')
 plt.legend(['training set', 'validation set'], loc='upper right')
 plt.savefig('model.png')
+
+print('Saved visualization data!')

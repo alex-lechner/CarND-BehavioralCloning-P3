28c7e0a53e6987f1a3251fb55a68c5923d583ab6
ALEX-PC\Alexa <office@alexanderlechner.com>
2017-07-26 23:17:55 +0200

    added better image preprocessing and modified model

--- model.py
+++ model.py
@@ -1,17 +1,19 @@
 import csv
+from pathlib import Path
 from random import shuffle
 
 import cv2
 import matplotlib
 import sklearn
+from keras.callbacks import ModelCheckpoint
 from sklearn.model_selection import train_test_split
 
 matplotlib.use('agg')
 
 import matplotlib.pyplot as plt
 import numpy as np
-from keras.layers import Flatten, Dense, Lambda, Cropping2D, Conv2D, Dropout, MaxPooling2D
-from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Conv2D, Dropout
+from keras.models import Sequential, model_from_json
 import json
 import matplotlib.image as mpimg
 
@@ -39,9 +41,8 @@ def generator(samples, batch_size=32):
                     # source_path has backslashes because of Windows
                     filename = source_path.split('\\')[-1]
                     current_path = './data/IMG/' + filename
-                    # image = cv2.imread(current_path)
                     image = mpimg.imread(current_path)
-                    image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+                    image = preprocess_img(image)
                     images.append(image)
                     measurement = float(batch_sample[3])
                     correction = 0.3
@@ -53,8 +54,8 @@ def generator(samples, batch_size=32):
                         measurement -= correction
                     measurements.append(measurement)
                     # augment data
-                    # images.append(np.fliplr(image))
-                    # measurements.append(measurement * -1.0)
+                    images.append(np.fliplr(image))
+                    measurements.append(measurement * -1.0)
 
             x_train = np.array(images)
             y_train = np.array(measurements)
@@ -62,41 +63,41 @@ def generator(samples, batch_size=32):
 
 
 def nvidia_cnn(dropout=0.3, epochs=3, batch_size=32):
-    input_shape = (160, 320, 3)
+    input_shape = (40, 160, 3)
     # Model architecture
     model = Sequential()
     # Normalizing data
-    model.add(Lambda(lambda x: (x / 127.) - 1., input_shape=input_shape))
-    # crop images so we only have the important parts (the street)
-    model.add(Cropping2D(cropping=((60, 20), (0, 0))))
+    model.add(Lambda(lambda x: (x / 127.5) - 1., input_shape=input_shape, output_shape=input_shape))
 
     # NVIDIA CNN
     model.add(Conv2D(24, (5, 5), activation="relu", strides=(2, 2)))
     model.add(Conv2D(36, (5, 5), activation="relu", strides=(2, 2)))
     model.add(Conv2D(48, (5, 5), activation="relu", strides=(2, 2)))
+    model.add(Conv2D(64, (3, 3), activation="relu", data_format="channels_first"))
     model.add(Conv2D(64, (3, 3), activation="relu"))
-    model.add(Conv2D(64, (3, 3), activation="relu"))
-    model.add(Dropout(dropout))
-    model.add(MaxPooling2D())
     model.add(Flatten())
-    model.add(Dense(100))
-    model.add(Dense(50))
-    model.add(Dense(10))
+    model.add(Dropout(dropout))
+    model.add(Dense(100, activation="relu"))
+    model.add(Dense(50, activation="relu"))
+    model.add(Dense(10, activation="relu"))
     model.add(Dense(1))
 
     model.summary()
 
     model.compile(loss='mse', optimizer='adam')
+
+    if Path("model.json").is_file() and Path("model.h5").is_file():
+        model = load_trained_model()
+
+    checkpoint = ModelCheckpoint(filepath='model.h5', save_best_only=True, monitor='val_loss')
     history_obj = model.fit_generator(train_generator, steps_per_epoch=int(len(train_samples) / batch_size),
                                       validation_data=validation_generator,
                                       validation_steps=int(len(validation_samples) / batch_size),
-                                      epochs=epochs)
+                                      epochs=epochs, callbacks=[checkpoint])
 
     # Save model
-    model.save('model.h5')
     with open('model.json', 'w') as f:
         json.dump(model.to_json(), f)
-
     print('Saved model!')
 
     # Save model data visualization
@@ -107,12 +108,30 @@ def nvidia_cnn(dropout=0.3, epochs=3, batch_size=32):
     plt.xlabel('epoch')
     plt.legend(['training set', 'validation set'], loc='upper right')
     plt.savefig('model.png')
-
     print('Saved visualization data!')
 
 
+def preprocess_img(image):
+    # resize to half of the original size (320x160 to 160x80)
+    h, w = image.shape[:2]
+    ratio = (w * 0.5) / w
+    dim = (int(w * 0.5), int(h * ratio))
+    image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+    # crop image to 160x40 (take away 30px from top and 10px from bottom) so we only have the important parts
+    image = image[30:70, 0:160]
+    return image
+
+
+def load_trained_model():
+    model = model_from_json(open('model.json').read())
+    # load weights into new model
+    model.load_weights("model.h5")
+    model.compile(loss='mse', optimizer='adam')
+    return model
+
+
 BATCH_SIZE = 64
-EPOCHS = 3
+EPOCHS = 5
 DROPOUT = 0.5
 
 train_generator = generator(train_samples, batch_size=BATCH_SIZE)
